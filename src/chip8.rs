26a0558870a6733c8a9fad2bcd9bdee845ea963f
Przemyslaw <przemek@gasinski.eu>
2022-06-01 22:38:45 +0200

    Add a UI

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,18 +1,6 @@
-use std::time::Duration;
-
 use crate::{
-    audio::Audio,
     instr::{bytes_to_word, split_into_4bits, Instructions},
     mem::Mem,
-};
-
-use sdl2::{
-    event::Event,
-    keyboard::{Keycode, Scancode},
-    pixels::Color,
-    rect::{Point, Rect},
-    video::Window,
-    Sdl,
 };
 
 pub const WIDTH: usize = 64;
@@ -29,24 +17,24 @@ _________________
 | Z | X | C | V |
 -----------------
 */
-const KEYMAP: [Scancode; 16] = [
-    Scancode::X,
-    Scancode::Num0,
-    Scancode::Num2,
-    Scancode::Num3,
-    Scancode::Q,
-    Scancode::W,
-    Scancode::E,
-    Scancode::A,
-    Scancode::S,
-    Scancode::D,
-    Scancode::Z,
-    Scancode::C,
-    Scancode::Num4,
-    Scancode::R,
-    Scancode::F,
-    Scancode::V,
-];
+// const KEYMAP: [Scancode; 16] = [
+//     Scancode::X,
+//     Scancode::Num0,
+//     Scancode::Num2,
+//     Scancode::Num3,
+//     Scancode::Q,
+//     Scancode::W,
+//     Scancode::E,
+//     Scancode::A,
+//     Scancode::S,
+//     Scancode::D,
+//     Scancode::Z,
+//     Scancode::C,
+//     Scancode::Num4,
+//     Scancode::R,
+//     Scancode::F,
+//     Scancode::V,
+// ];
 
 pub struct Chip8 {
     pub v: [u8; 16],
@@ -82,111 +70,43 @@ impl Chip8 {
     }
 
     pub fn main_loop(&mut self) {
-        let (sdl_context, window, audio) = self.init_sdl();
-
-        let mut canvas = window.into_canvas().build().unwrap();
-
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-        canvas.clear();
-        canvas.present();
-
-        let mut event_pump = sdl_context.event_pump().unwrap();
-
-        let lines = [
-            Point::new(0, (HEIGHT * 4) as i32 + 1),
-            Point::new((WIDTH * 4) as i32 + 1, (HEIGHT * 4) as i32 + 1),
-            Point::new((WIDTH * 4) as i32 + 1, 0),
-        ];
-
-        let ttf = sdl2::ttf::init().unwrap();
-        let font = ttf.load_font("assets/OpenSans-Regular.ttf", 14).unwrap();
-        let texture_creator = canvas.texture_creator();
+        // let lines = [
+        //     Point::new(0, (HEIGHT * 4) as i32 + 1),
+        //     Point::new((WIDTH * 4) as i32 + 1, (HEIGHT * 4) as i32 + 1),
+        //     Point::new((WIDTH * 4) as i32 + 1, 0),
+        // ];
 
         'running: loop {
-            for event in event_pump.poll_iter() {
-                match event {
-                    Event::Quit { .. }
-                    | Event::KeyDown {
-                        keycode: Some(Keycode::Escape),
-                        ..
-                    } => break 'running,
-                    _ => {}
-                }
-            }
+            // for (i, scancode) in KEYMAP.iter().enumerate() {
+            //     self.keys[i] = event_pump.keyboard_state().is_scancode_pressed(*scancode);
 
-            canvas.clear();
-
-            for (i, scancode) in KEYMAP.iter().enumerate() {
-                self.keys[i] = event_pump.keyboard_state().is_scancode_pressed(*scancode);
-
-                if self.paused && self.keys[i] {
-                    self.v[self.target_register.unwrap()] = i as u8;
-                    self.target_register = None;
-                    self.paused = false;
-                }
-            }
-
-            if !self.paused {
-                self.execute();
-                self.tick_timers(&audio);
-            }
-
-            canvas.set_draw_color(Color::WHITE);
-
-            // Draw pixels
-            for y in 0..HEIGHT {
-                for x in 0..WIDTH {
-                    if self.gfx[y * WIDTH + x] {
-                        canvas
-                            .fill_rect(Rect::new(x as i32 * 4, y as i32 * 4, 4, 4))
-                            .unwrap();
-                    }
-                }
-            }
-
-            draw_ui(&mut canvas, lines, &font, &texture_creator, &self.v);
-
-            canvas.set_draw_color(Color::BLACK);
-            canvas.present();
-
-            ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 500));
+            //     if self.paused && self.keys[i] {
+            //         self.v[self.target_register.unwrap()] = i as u8;
+            //         self.target_register = None;
+            //         self.paused = false;
+            //     }
+            // }
         }
     }
 
-    fn tick_timers(&mut self, audio: &Audio) {
+    fn tick_timers(&mut self) {
         if self.delay_timer > 0 {
             self.delay_timer -= 1;
         }
 
         if self.sound_timer > 0 {
-            audio.start_beep();
+            // audio.start_beep();
             self.sound_timer -= 1;
         } else {
-            audio.stop_beep();
+            // audio.stop_beep();
         }
     }
 
-    fn init_sdl(&mut self) -> (Sdl, Window, Audio) {
-        let sdl_context = sdl2::init().unwrap();
+    pub fn execute(&mut self) {
+        if self.paused {
+            return;
+        }
 
-        let video_subsystem = sdl_context.video().unwrap();
-
-        let window = video_subsystem
-            // .window("chipr", 4 * WIDTH as u32, 4 * HEIGHT as u32)
-            // TODO Resize properly
-            .window("chipr", 800, 600)
-            .position_centered()
-            .build()
-            .unwrap();
-
-        let _audio_context = sdl_context.audio().unwrap();
-
-        let audio = Audio::new(&sdl_context);
-
-        (sdl_context, window, audio)
-    }
-
-    fn execute(&mut self) {
         let (h, l) = (self.mem.get(self.pc), self.mem.get(self.pc + 1));
         let opcode = bytes_to_word(h, l);
 
@@ -231,37 +151,7 @@ impl Chip8 {
 
             _ => panic!("Unimplemented: {:#06X}", opcode),
         }
-    }
-}
 
-fn draw_ui(
-    canvas: &mut sdl2::render::Canvas<Window>,
-    lines: [Point; 3],
-    font: &sdl2::ttf::Font,
-    texture_creator: &sdl2::render::TextureCreator<sdl2::video::WindowContext>,
-    v: &[u8; 16],
-) {
-    canvas.set_draw_color(Color::GRAY);
-
-    // Draw the border of the game screen
-    canvas.draw_lines(&lines[..]).unwrap();
-
-    // Draw register values
-    for i in 0..16 {
-        // TODO Optimize font drawing
-        let font_result = font
-            .render(format!("V{:X}: {:02X}", i, v[i]).as_str())
-            .solid(Color::WHITE)
-            .unwrap();
-
-        let texture = texture_creator
-            .create_texture_from_surface(font_result)
-            .unwrap();
-
-        let x = 100 * (i as i32 % 5);
-        let y = 50 * (i as i32 / 5);
-        canvas
-            .copy(&texture, None, Some(Rect::new(x + 300, y, 50, 20)))
-            .unwrap();
+        self.tick_timers();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,105 @@
+use eframe::egui::{Context, Sense, Visuals};
+use eframe::emath::pos2;
+use eframe::epaint::{Color32, Pos2, Rect, Rounding, Vec2};
+use eframe::{egui, App, Frame, NativeOptions};
 use std::io::prelude::*;
+use std::path::PathBuf;
 use std::{fs::File, io::BufReader};
 
-use chip8::Chip8;
+use chip8::{Chip8, HEIGHT, WIDTH};
 use mem::Mem;
 
-mod audio;
 mod chip8;
 mod instr;
 mod mem;
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = std::env::args().collect();
+    eframe::run_native(
+        "Chipr",
+        NativeOptions::default(),
+        Box::new(|cc| Box::new(Chip8Emu::new(cc))),
+    );
+}
 
-    if args.len() != 2 {
-        println!("Missing path to rom.\nUsage: chipr rom_path");
-        std::process::exit(-1);
+struct Chip8Emu {
+    rom_path: Option<PathBuf>,
+    chip8: Option<Chip8>,
+}
+
+impl Chip8Emu {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        cc.egui_ctx.set_visuals(Visuals::dark());
+
+        Self {
+            rom_path: None,
+            chip8: None,
+        }
     }
 
-    let file = File::open(args.get(1).unwrap())?;
-    let mut buf_reader = BufReader::new(file);
-    let mut buffer = Vec::new();
-    buf_reader.read_to_end(&mut buffer)?;
+    fn start_chip8(&mut self) {
+        let path = self.rom_path.clone().unwrap();
+        let file = File::open(path).unwrap();
+        let mut buf_reader = BufReader::new(file);
+        let mut buffer = Vec::new();
+        buf_reader.read_to_end(&mut buffer).unwrap();
 
-    let mut mem = Mem::new();
-    mem.load_rom(buffer);
+        let mut mem = Mem::new();
+        mem.load_rom(buffer);
 
-    let mut chip8 = Chip8::new(mem);
+        self.chip8 = Some(Chip8::new(mem));
+    }
+}
 
-    chip8.main_loop();
+impl App for Chip8Emu {
+    fn update(&mut self, ctx: &Context, frame: &mut Frame) {
+        if ctx.input().key_pressed(egui::Key::Escape) {
+            frame.quit();
+        }
 
-    Ok(())
+        egui::SidePanel::left("load_panel").show(ctx, |ui| {
+            ui.heading("Menu");
+
+            if ui.button("Open ROM").clicked() {
+                if let Some(path) = rfd::FileDialog::new().pick_file() {
+                    self.rom_path = Some(path.clone());
+                    self.start_chip8();
+                }
+            }
+
+            if ui.button("Reset ROM").clicked() {
+                println!("PATH: {:?}", self.rom_path);
+                if self.rom_path.is_some() {
+                    self.start_chip8();
+                }
+            }
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // Draw the game
+            let (response, painter) = ui.allocate_painter(
+                Vec2::new((WIDTH * 4) as f32, (HEIGHT * 4) as f32),
+                Sense::hover(),
+            );
+
+            if let Some(chip8) = &mut self.chip8 {
+                chip8.execute();
+
+                for y in 0..HEIGHT {
+                    for x in 0..WIDTH {
+                        if chip8.gfx[y * WIDTH + x] {
+                            painter.rect_filled(
+                                Rect::from_min_size(
+                                    response.rect.left_top()
+                                        + Vec2::new((x * 4) as f32, (y * 4) as f32),
+                                    Vec2::splat(4.0),
+                                ),
+                                Rounding::none(),
+                                Color32::WHITE,
+                            );
+                        }
+                    }
+                }
+            }
+        });
+    }
 }
